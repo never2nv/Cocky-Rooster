51e2764a4e75f72d2ba4b7d5479510243dd26db4
never2nv <FNGnation@gmail.com>
2013-06-05 14:08:34 -0400

    Added 1.5.2 DevBuild

--- src/me/never2nv/CockyRooster/CockyRooster.java
+++ src/me/never2nv/CockyRooster/CockyRooster.java
@@ -2,6 +2,7 @@ package me.never2nv.CockyRooster;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CockyRooster extends JavaPlugin{
 	public final Logger logger = Logger.getLogger("Minecraft)");
+	public String cocky = ChatColor.RED + "[" + ChatColor.YELLOW + "CockyRooster" + ChatColor.RED + "] " + ChatColor.WHITE;
 	public static CockyRooster plugin;
 	
 	@Override
@@ -34,31 +36,32 @@ public class CockyRooster extends JavaPlugin{
 			// Sends pm to user who triggered the cmd.
 			player.sendMessage(ChatColor.GREEN + "You sure do look great today! So intelligent and handsome! Wow, I'm telling everyone!");
 			// Sends global message to the server as the user indicated above.
-			player.chat("/me " + ChatColor.RED + "<-- Look at this handsome, intelligent, courageous individual right " + ChatColor.GREEN + "HERE!");
+			Bukkit.broadcastMessage(ChatColor.YELLOW + player.getName() + ChatColor.RED + " is such a handsome, intelligent, courageous individual!" + ChatColor.GREEN + " RESPECT!");
 		}
 	    if(commandLabel.equalsIgnoreCase("smart")){
 	    	// Sends pm to user who triggered the cmd.
 	    	player.sendMessage(ChatColor.GREEN + "You are so intelligent! You make Bill Nye look like child's play! I'm informing others...");
 	    	// Sends global message to the server as the user indicated above.
-	    	player.chat("/me " + ChatColor.WHITE + "just solved an impossible complex algorithm " + ChatColor.RED + "Making them the smartest person on the server!");
+	    	Bukkit.broadcastMessage(ChatColor.GREEN + player.getName() + ChatColor.YELLOW + " just solved a complex algirithim!" + ChatColor.RED + " Making them the most intelligent person on this server!");
 	    }
 	    if(commandLabel.equalsIgnoreCase("l33t")){
 	    	// Sends pm to user who triggered the cmd.
 			player.sendMessage(ChatColor.GREEN + "Telling others what a hax0r you are!");
 			// Sends global message to the server as the user indicated above.
-			player.chat("/me " + ChatColor.GREEN + "Can open up any command line terminal and access " + ChatColor.GREEN + "ANYTHING! Such a l33t hax0r!");
+			Bukkit.broadcastMessage(ChatColor.RED + player.getName() + ChatColor.GREEN + " Can open up any command line terminal and access: " + ChatColor.YELLOW + "ANYTHING! Such a l33t hax0r!");
 		}
 	    if(commandLabel.equalsIgnoreCase("elite")){
 	    	// Sends pm to user who triggered the cmd.
 			player.sendMessage(ChatColor.GREEN + "Telling others of your greatness!");
 			// Sends global message to the server as the user indicated above.
-			player.chat("/me " + ChatColor.GREEN + "Is so elite: " + ChatColor.RED + "He punched an enderdragon to death, literally..... with their fists!");
+			
+			Bukkit.broadcastMessage(ChatColor.GREEN + player.getName() + ChatColor.YELLOW + " is SO elite: " + ChatColor.RED + " they punched an enderdragon to death, literally..... with their fists!");
 	    }
 	    if(commandLabel.equalsIgnoreCase("troll")){
 	    	// Sends pm to user who triggered the cmd.
 			player.sendMessage(ChatColor.GREEN + "Telling others of your greatness!");
 			// Sends global message to the server as the user indicated above.
-			player.chat("/me " + ChatColor.GREEN + "SHUTUP! " + ChatColor.RED + "GET BACK UNDER YOUR BRIDGE TROLL!");
+			Bukkit.broadcastMessage(ChatColor.GREEN + player.getName() + ChatColor.YELLOW + " says to SHUTUP!! " + ChatColor.RED + " AN' GET BACK UNDER YOUR BRIDGE TROLL!");
 	    }
 	    // Return statement, derp.
 		return false;
